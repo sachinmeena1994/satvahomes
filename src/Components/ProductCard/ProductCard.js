32089c61ce6a60a5fb8f717c61ce2d4a61538b70
Rishabh Malewar <rishumalewar@gmail.com>
2024-05-28 10:12:53 +0530

    some changes in product card to make it user interactive

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./productCard.css";
 
-function ProductCard({ image, title }) {
+function ProductCard({ image, title, description }) {
+  const truncateDescription = (desc, length) => {
+    return desc.length > length ? desc.substring(0, length) + "..." : desc;
+  };
+
   return (
     <div className="card">
       <img className="card-image" src={image} alt={title} />
       <div className="card-content">
         <p className="card-title">{title}</p>
+      </div>
+      <div className="card-overlay">
+        <p className="card-title">{title}</p>
+        <p className="card-description">{truncateDescription(description, 50)}</p>
       </div>
     </div>
   );
