b8fc02accc1c2b6fac461cd2163f1c45619fdb59
Vishwesh Dhoble <vishweshdhoble0610@gmail.com>
2024-06-04 08:57:43 +0530

    done

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -138,7 +138,7 @@ const Contact = () => {
                 type="email"
                 id="email"
                 class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light outline-none"
-                placeholder="name@flowbite.com"
+                placeholder="name@gmail.com"
                 required
               />
             </div>
@@ -168,7 +168,7 @@ const Contact = () => {
                 id="message"
                 rows="6"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 resize-none outline-none"
-                placeholder="Leave a comment..."
+                placeholder="Write a message..."
               ></textarea>
             </div>
             <button
