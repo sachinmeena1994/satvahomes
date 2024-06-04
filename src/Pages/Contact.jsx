e0c3ad29107ee462efa4f598b5bc64805fbc16cc
Vishwesh Dhoble <vishweshdhoble0610@gmail.com>
2024-06-04 19:50:01 +0530

    contact form changed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -129,14 +129,14 @@ const Contact = () => {
             <div>
               <label
                 for="name"
-                class="block mb-2 text-sm font-medium text-zinc-700 dark:text-gray-300"
+                class="block mb-2 text-sm font-medium text-zinc-700"
               >
                 Name
               </label>
               <input
                 type="text"
                 id="name"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light outline-none"
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 outline-none"
                 placeholder="your name"
                 required
               />
@@ -144,14 +144,14 @@ const Contact = () => {
             <div>
               <label
                 for="email"
-                class="block mb-2 text-sm font-medium text-zinc-700 dark:text-gray-300"
+                class="block mb-2 text-sm font-medium text-zinc-700"
               >
                 Your email
               </label>
               <input
                 type="email"
                 id="email"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light outline-none"
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 outline-none"
                 placeholder="name@gmail.com"
                 required
               />
@@ -159,14 +159,14 @@ const Contact = () => {
             <div>
               <label
                 for="subject"
-                class="block mb-2 text-sm font-medium text-zinc-700 dark:text-gray-300"
+                class="block mb-2 text-sm font-medium text-zinc-700"
               >
                 Subject
               </label>
               <input
                 type="text"
                 id="subject"
-                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light outline-none"
+                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 outline-none"
                 placeholder="Let us know how we can help you"
                 required
               />
@@ -174,14 +174,14 @@ const Contact = () => {
             <div class="sm:col-span-2">
               <label
                 for="message"
-                class="block mb-2 text-sm font-medium text-zinc-700 dark:text-gray-400"
+                class="block mb-2 text-sm font-medium text-zinc-700"
               >
                 Your message ( optional )
               </label>
               <textarea
                 id="message"
                 rows="6"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 resize-none outline-none"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 resize-none outline-none"
                 placeholder="Write a message..."
               ></textarea>
             </div>
