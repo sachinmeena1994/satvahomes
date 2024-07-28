8fd84efa3d90e514684e7a7eddca6c210dc3909d
Vishwesh Dhoble <vishweshdhoble0610@gmail.com>
2024-07-29 00:35:13 +0530

    minor tweaks and vendor admin panel changes

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -13,6 +13,14 @@ import VendorDashboard from "../Components/VendorDashboard";
 import CreateProduct from "../Components/CreateProduct";
 import ProductManager from "../Components/ProductManager";
 import BulkUpload from "../Components/BulkUpload";
+import {
+  FaUsers,
+  FaPlusCircle,
+  FaBullhorn,
+  FaBoxOpen,
+  FaUpload,
+} from "react-icons/fa";
+import { FaAd, FaProductHunt } from "react-icons/fa";
 
 function Admin() {
   const [menuOption, setMenuOption] = useState("");
@@ -72,57 +80,63 @@ function Admin() {
         className="w-1/4 bg-gradient-to-r from-[#0E6D55] to-[#1cca9e] p-6 text-white"
         style={{ height: "100vh" }}
       >
-        <ul className="mt-4">
+        <ul className="mt-4 space-y-2">
           <li
-            className={`cursor-pointer py-2 px-4 rounded-lg mb-2 duration-200 ${
+            className={`cursor-pointer py-3 px-4 rounded-lg flex items-center space-x-3 duration-200 ${
               menuOption === "users" ? "bg-[#174f41]" : "hover:bg-[#174f4166]"
             }`}
             onClick={() => setMenuOption("users")}
           >
-            Users
+            <FaUsers className="text-lg" />
+            <span>Users</span>
           </li>
           <li
-            className={`cursor-pointer py-2 px-4 duration-200 rounded-lg mb-2 ${
+            className={`cursor-pointer py-3 px-4 rounded-lg flex items-center space-x-3 duration-200 ${
               menuOption === "createProduct"
                 ? "bg-[#174f41]"
-                : "hover:bg-[#0E6D55]"
+                : "hover:bg-[#174f4166]"
             }`}
             onClick={() => setMenuOption("createProduct")}
           >
-            Create Product
+            <FaPlusCircle className="text-lg" />
+            <span>Create Product</span>
           </li>
           <li
-            className={`cursor-pointer py-2 px-4 duration-200 rounded-lg mb-2 ${
+            className={`cursor-pointer py-3 px-4 rounded-lg flex items-center space-x-3 duration-200 ${
               menuOption === "advertisement"
                 ? "bg-[#174f41]"
-                : "hover:bg-[#0E6D55]"
+                : "hover:bg-[#174f4166]"
             }`}
             onClick={() => setMenuOption("advertisement")}
           >
-            Vendor
+            <FaAd className="text-lg" />
+            <span>Vendor</span>
           </li>
           <li
-            className={`cursor-pointer py-2 px-4 duration-200 rounded-lg mb-2 ${
+            className={`cursor-pointer py-3 px-4 rounded-lg flex items-center space-x-3 duration-200 ${
               menuOption === "manageProducts"
                 ? "bg-[#174f41]"
-                : "hover:bg-[#0E6D55]"
+                : "hover:bg-[#174f4166]"
             }`}
             onClick={() => setMenuOption("manageProducts")}
           >
-            Manage Products
+            <FaBoxOpen className="text-lg" />
+            <span>Manage Products</span>
           </li>
           <li
-            className={`cursor-pointer py-2 px-4 duration-200 rounded-lg mb-2 ${
+            className={`cursor-pointer py-3 px-4 rounded-lg flex items-center space-x-3 duration-200 ${
               menuOption === "bulkUpload"
                 ? "bg-[#174f41]"
-                : "hover:bg-[#0E6D55]"
+                : "hover:bg-[#174f4166]"
             }`}
             onClick={() => setMenuOption("bulkUpload")}
           >
-            Bulk Upload
+            <FaUpload className="text-lg" />
+            <span>Bulk Upload</span>
           </li>
         </ul>
       </aside>
+
       <main
         className="w-3/4 overflow-y-auto  bg-gray-50 p-6"
         style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
@@ -130,64 +144,68 @@ function Admin() {
         <div>
           {menuOption === "users" && (
             <div>
-            {users.map((user, index) => (
-              <div
-                key={index}
-                className="bg-white shadow-md rounded-lg p-6 mb-6 duration-200 transition-all transform hover:scale-102"
-              >
-                <h3
-                  className="text-xl font-semibold cursor-pointer flex items-center justify-between"
-                  onClick={() => toggleUserDetails(index)}
-                >
-                  Name : {user.name}
-                  <FaChevronUp
-                    className={`ml-1 ${
-                      expandedUserIndex === index
-                        ? "rotate-180 duration-300 text-[#0E6D55]"
-                        : "duration-500"
-                    }`}
-                  />
-                </h3>
+              {users.map((user, index) => (
                 <div
-                  className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
-                    expandedUserIndex === index ? "max-h-screen mt-4" : "max-h-0"
-                  }`}
+                  key={index}
+                  className="bg-white shadow-md rounded-lg p-6 mb-6 duration-200 transition-all transform hover:scale-102"
                 >
-                  <div className="bg-gray-100 flex gap-1 p-4 mb-4 rounded-lg">
-                    <h4 className="text-md font-semibold mb-2">Name :</h4>
-                    <p>{user.name}</p>
-                  </div>
-                  <div className="bg-gray-100 flex gap-1 p-4 mb-4 rounded-lg">
-                    <h4 className="text-md font-semibold mb-2">Email :</h4>
-                    <p>{user.email}</p>
-                  </div>
-                  <div className="bg-gray-100 flex gap-2 items-center p-4 mb-4 rounded-lg">
-                    <h4 className="text-md font-semibold mb-2">Role :</h4>
-                    <div className="flex items-center">
-                      <div className="pl-2 border border-zinc-200 rounded mr-2 bg-white">
-                        <select
-                          className="rounded-md py-2 pb-2 px-2 mr-4 border-none outline-none"
-                          value={user.role}
-                          onChange={(e) => handleEditRole(index, e.target.value)}
+                  <h3
+                    className="text-xl font-semibold cursor-pointer flex items-center justify-between"
+                    onClick={() => toggleUserDetails(index)}
+                  >
+                    Name : {user.name}
+                    <FaChevronUp
+                      className={`ml-1 ${
+                        expandedUserIndex === index
+                          ? "rotate-180 duration-300 text-[#0E6D55]"
+                          : "duration-500"
+                      }`}
+                    />
+                  </h3>
+                  <div
+                    className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
+                      expandedUserIndex === index
+                        ? "max-h-screen mt-4"
+                        : "max-h-0"
+                    }`}
+                  >
+                    <div className="bg-gray-100 flex gap-1 p-4 mb-4 rounded-lg">
+                      <h4 className="text-md font-semibold mb-2">Name :</h4>
+                      <p>{user.name}</p>
+                    </div>
+                    <div className="bg-gray-100 flex gap-1 p-4 mb-4 rounded-lg">
+                      <h4 className="text-md font-semibold mb-2">Email :</h4>
+                      <p>{user.email}</p>
+                    </div>
+                    <div className="bg-gray-100 flex gap-2 items-center p-4 mb-4 rounded-lg">
+                      <h4 className="text-md font-semibold mb-2">Role :</h4>
+                      <div className="flex items-center">
+                        <div className="pl-2 border border-zinc-200 rounded-lg mr-2 bg-white">
+                          <select
+                            className="rounded-lg py-2 pb-2 px-2 mr-4 border-none outline-none"
+                            value={user.role}
+                            onChange={(e) =>
+                              handleEditRole(index, e.target.value)
+                            }
+                          >
+                            <option value="user">User</option>
+                            <option value="admin">Admin</option>
+                            <option value="vendor">Vendor</option>
+                            <option value="designer">Designer</option>
+                          </select>
+                        </div>
+                        <button
+                          className="bg-[#0e6d55e1] text-white px-4 py-2 duration-300 rounded-lg hover:bg-green-900 outline-none"
+                          onClick={() => handleUpdateRole(index)}
                         >
-                          <option value="user">User</option>
-                          <option value="admin">Admin</option>
-                          <option value="vendor">Vendor</option>
-                          <option value="designer">Designer</option>
-                        </select>
+                          Update
+                        </button>
                       </div>
-                      <button
-                        className="bg-[#0e6d55e1] text-white px-4 py-2 duration-300 rounded hover:bg-green-900 outline-none"
-                        onClick={() => handleUpdateRole(index)}
-                      >
-                        Update
-                      </button>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
           )}
           {menuOption === "advertisement" && (
             <div>
